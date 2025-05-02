dfe4104115daac8b6fc963e5e3c9910d48043152
TharunGopal-Agraharam <136571842+TharunGopal-Agraharam@users.noreply.github.com>
2025-05-02 13:30:43 +0530

    Delete planner_agent.py

--- planner_agent.py
+++
@@ -1,9 +0,0 @@
-class PlannerAgent:
-    def create_plan(self, goal):
-        # Break down high-level goal into steps
-        plan = {
-            "step1": "Define patient cohort criteria",
-            "step2": "Fetch patient data from FHIR API",
-            "step3": "Send outreach emails"
-        }
-        return plan
